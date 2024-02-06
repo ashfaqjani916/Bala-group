564b6f69d318c30bd28e85816d9397adc61e3dff
asfaqjani916 <ashfaqjani916@gmail.com>
2024-02-06 15:09:02 +0530

    [Feat] fetching data from DB

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -1,10 +1,46 @@
 import "../css/publications.css"
+import { useEffect,useState } from "react";
+
+
+interface Publications{
+  title: string;
+  journal: string;
+  doi: string;
+}
+
 const Publications = () => {
-  return (
+
+  const [publications, setPublications] = useState<Publications[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("data/db.json");
+        const data = await response.json();
+        setPublications(data.publications);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures this effect runs only once on mount
+
+  
+  return (  
     <div className="works">
-      Publication
-      
-    </div>
+    <h1>Your Publications</h1>
+    <ul>
+      {publications.map((publication, index) => (
+        <li key={index}>
+          <a href="publication.doi"></a>
+          <strong>{publication.title}</strong>
+          <p>Journal: {publication.journal}</p>
+          {/* ... other fields */}
+        </li>
+      ))}
+    </ul>
+  </div>
   )
 }
 
